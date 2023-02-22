4a5658b122d7f1dd3f11e23dde73df02c3f886b1
Ayesha Ilyas <annie16950@gmail.com>
2023-02-21 19:07:09 -0500

    Create models for basic app entities (AppError, Item, User, Wishlist)

---
+++ src/main/java/edu/sunyulster/genie/models/Item.java
@@ -0,0 +1,57 @@
+package edu.sunyulster.genie.models;
+
+public class Item {
+    private String name;
+    private double price;
+    private String supplier;
+    private boolean purchased;
+
+    public Item() {
+
+    }
+
+    public Item(String name, double price, String supplier) {
+        this.name = name;
+        this.price = price;
+        this.supplier = supplier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPerchased(boolean purchased) {
+        this.purchased = purchased;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Item Name: %s%nPrice: %.2d%nSupplier: %s%n", 
+            name, price, supplier); 
+    }
+}

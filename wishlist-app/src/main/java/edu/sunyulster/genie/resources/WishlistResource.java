d09e46e8910641b0af3fbfad95c06bfbadee664e
Ayesha Ilyas <annie16950@gmail.com>
2023-03-26 14:12:12 -0400

    Create wishlist service and resource

---
+++ wishlist-app/src/main/java/edu/sunyulster/genie/resources/WishlistResource.java
@@ -0,0 +1,94 @@
+package edu.sunyulster.genie.resources;
+
+import java.util.List;
+
+import edu.sunyulster.genie.exceptions.InvalidDataException;
+import edu.sunyulster.genie.models.AppError;
+import edu.sunyulster.genie.models.Wishlist;
+import edu.sunyulster.genie.services.WishlistService;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.AuthenticationException;
+import jakarta.websocket.server.PathParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.core.UriInfo;
+
+@RequestScoped
+@Path("/wishlists")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+// @RolesAllowed({"user"})
+public class WishlistResource {
+    
+    // @Inject
+    // @Claim("sub")
+    // private String userId;
+
+    @Inject
+    private WishlistService wishlistService;
+
+    @QueryParam("id")
+    private String userId;
+    
+    @POST
+    public Response createWishlist(Wishlist wishlist) throws InvalidDataException {
+        Wishlist newWishlist = wishlistService.create(userId, wishlist);
+        return Response.ok()
+            .entity(newWishlist)
+            .build();
+    }
+
+    @GET
+    public Response getWishlists() throws AuthenticationException {
+        List<Wishlist> wishlists = wishlistService.getAll(userId);
+        return Response.ok()
+            .entity(wishlists)
+            .build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getWishlist(@PathParam("id") String id) throws AuthenticationException {
+        Wishlist wishlists = wishlistService.get(userId, id);
+        return Response.ok()
+            .entity(wishlists)
+            .build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    // why doest Wishlist wishlist, @PathParam("id") String id work
+    // ERROR: Resource methods cannot have more than one entity parameterjakarta-jax_rs(ResourceMethodMultipleEntityParams)
+    public Response updateWishlist(Wishlist wishlist, @Context UriInfo uriInfo) throws InvalidDataException {
+        List<String> ids = uriInfo.getPathParameters().get("id");
+        if (ids == null)
+            return Response.status(Status.BAD_REQUEST)
+                .entity(new AppError(400, "Must include wishlist id in request as path param"))
+                .build();
+        
+        wishlist.setId(ids.get(0));
+        Wishlist updatedWishlist = wishlistService.update(userId, wishlist);
+        return Response.ok()
+            .entity(updatedWishlist)
+            .build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteWishlist(@PathParam("id") String id) {
+        wishlistService.delete(userId, id);
+        return Response.noContent().build();
+    }
+
+}
---
+++ wishlist-app/src/main/java/edu/sunyulster/genie/services/WishlistService.java
@@ -0,0 +1,143 @@
+package edu.sunyulster.genie.services;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.in;
+import static edu.sunyulster.genie.utils.Validator.isWishlistValid;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+
+import edu.sunyulster.genie.exceptions.InvalidDataException;
+import edu.sunyulster.genie.models.Wishlist;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.security.enterprise.AuthenticationException;
+import jakarta.ws.rs.ForbiddenException;
+
+
+@ApplicationScoped
+public class WishlistService {
+
+    @Inject
+    MongoDatabase db;
+
+    public Wishlist create(String userId, Wishlist w) throws InvalidDataException {
+        // validate information
+        if (!isWishlistValid(w)) 
+            throw new InvalidDataException("Wishlist must have a name");
+
+        // create wishlist
+        Document newWishlist = new Document()
+            .append("_id", new ObjectId())
+            .append("name", w.getName())
+            .append("items", new ArrayList<ObjectId>())
+            .append("dateCreated", new Date());
+
+        // add to wishlist collection
+        MongoCollection<Document> wishlists = db.getCollection("wishlists");
+        wishlists.insertOne(newWishlist);
+
+        // add wishlist id to user
+        MongoCollection<Document> users = db.getCollection("users");
+        Bson update = Updates.addToSet("wishlists", newWishlist.getObjectId("_id"));
+        users.updateOne(eq("authId", userId), update, null);
+
+        return documentToWishlist(newWishlist);
+    }
+
+    public List<Wishlist> getAll(String userId) throws AuthenticationException {
+        // get all wishlists for the user 
+        MongoCollection<Document> users = db.getCollection("users");
+        Document match = users.find(eq("authId", userId)).first();
+        // occurs if user is deleted but still has valid jwt for example
+        if (match == null)
+            throw new AuthenticationException("User does not exist!");
+
+        List<ObjectId> wishlistIds = (ArrayList<ObjectId>) match.get("wishlists");
+        MongoCollection<Document> wishlistCol = db.getCollection("wishlists");
+        FindIterable<Document> matchingWishlists = wishlistCol.find(in("_id", wishlistIds))
+            .sort(Sorts.ascending("dateCreated"));
+
+        List<Wishlist> wishlists = new ArrayList<>(wishlistIds.size());
+        for (Document w : matchingWishlists) 
+            wishlists.add(documentToWishlist(w));
+        
+        return wishlists;
+    }
+
+    public Wishlist get(String userId, String id) {
+        ObjectId wishlisId = new ObjectId(id);
+        checkWishlistOwnsership(new ObjectId(userId), wishlisId);
+        
+        MongoCollection<Document> wishlists = db.getCollection("wishlists");
+        Document match = wishlists.find(eq("_id", wishlisId)).first();
+        if (match == null) 
+            throw new NoSuchElementException("Wishlist does not exist");
+        return documentToWishlist(match);
+    }
+
+    public Wishlist update(String userId, Wishlist newWishlist) throws InvalidDataException {
+        ObjectId wishlistId = new ObjectId(newWishlist.getId());
+        checkWishlistOwnsership(new ObjectId(userId), wishlistId);
+        
+         // validate information
+         if (!isWishlistValid(newWishlist)) 
+            throw new InvalidDataException("Wishlist must have a name");
+
+        // get updated info
+        Document updatedWishlist = new Document().append("name", newWishlist.getName());
+
+        // replace previous wishlist data with new data
+        MongoCollection<Document> wishlists = db.getCollection("wishlists");
+        UpdateResult result = wishlists.replaceOne(eq("_id", wishlistId), updatedWishlist);
+        // throw exception if the wishlist does not exist
+        if (result.getModifiedCount() == 0)
+            throw new NoSuchElementException("Wishlist does not exists");
+        
+        return documentToWishlist(wishlists.find(eq("_id", wishlistId)).first());
+    }
+
+    public void delete(String userId, String id) {
+        ObjectId wishlistId = new ObjectId(id);
+        checkWishlistOwnsership(new ObjectId(userId), wishlistId);
+
+        MongoCollection<Document> wishlists = db.getCollection("wishlists");
+        DeleteResult result = wishlists.deleteOne(eq("_id", wishlistId));
+        if (result.getDeletedCount() == 0) 
+            throw new NoSuchElementException("Wishlist does not exists");
+    }
+
+    private Wishlist documentToWishlist(Document d) {
+        int itemCount = ((ArrayList<ObjectId>) d.get("items")).size();
+        return new Wishlist(d.getObjectId("_id").toString(), 
+            d.getString("name"), 
+            itemCount, 
+            d.getDate("dateCreated"));
+    }
+
+    private Document checkWishlistOwnsership(ObjectId userId, ObjectId wishlistId) {
+        MongoCollection<Document> users = db.getCollection("users");
+        Document match = users.find(eq("authId", userId)).first();
+        List<ObjectId> wishlistIds = (ArrayList<ObjectId>) match.get("wishlists");
+        for (ObjectId i : wishlistIds) {
+            if (i.equals(wishlistId))
+                return match;
+        }
+        throw new ForbiddenException("Wishlist does not belong to requesting user");
+    }
+    
+}

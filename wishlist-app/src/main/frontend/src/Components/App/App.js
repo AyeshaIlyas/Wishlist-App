8f1e4097afd135b283a97b70f8ecfbc2b9ebeec6
013MSI <69552976+013MSI@users.noreply.github.com>
2023-03-27 16:29:20 -0400

    Updated Home and Footer

--- wishlist-app/src/main/frontend/src/Components/App/App.js
+++ wishlist-app/src/main/frontend/src/Components/App/App.js
@@ -1,41 +1,26 @@
 import { Route, Routes, NavLink } from 'react-router-dom';
 import {  useState } from 'react';
 import './App.css';
+import Navbar from "./../Navbar/Navbar";
 import Home from "./../Home/Home";
 import Register from "./../Register/Register";
 import Login from "./../Login/Login";
 import Profile from "./../Profile/Profile";
 import MyWishlists from "./../MyWishlists/MyWishLists";
+import Footer from "./../Footer/Footer";
 import Protected from "./../Utils/Protected";
 import { loggedIn } from '../../utils/auth';
-import Logout from '../Logout/Logout';
 
 
 function App() {
 
   // TODO: logout when JWT expires - check if token expired whever user tries to access new route
   const [verified, setVerified]= useState();
-
-  let nav;
-  if (!loggedIn()) {
-    nav = <nav>
-      <NavLink to="/" style={{ padding: "10px" }}>Home</NavLink>
-      <NavLink to="/login" style={{ padding: "10px" }}>Login</NavLink>
-      <NavLink to="/register" style={{ padding: "10px" }}>Register</NavLink>
-    </nav>
-  } else {
-    nav = <nav>
-      <NavLink to="/" style={{ padding: "10px" }}>Home</NavLink>
-      <NavLink to="/profile" style={{ padding: "10px" }}>Profile</NavLink>
-      <NavLink to="/wishlists" style={{ padding: "10px" }}>My Wishlists</NavLink>
-      <NavLink to="/shared" style={{ padding: "10px" }}>Shared With Me</NavLink>
-      <Logout setVerified={setVerified}/>
-    </nav>
-  }
+ 
 
   return (
     <div className="App">
-        {nav}
+        <Navbar isLoggedIn={verified}/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/register" element={<Register/>}/>
@@ -57,6 +42,7 @@ function App() {
           }/>
           <Route path="*" element={<p>Hmmm no idea what youre asking for...</p>}/>
         </Routes>
+        <Footer />
     </div>
   );
 }
---
+++ wishlist-app/src/main/frontend/src/Components/Home/FeatureDeck.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import Feature from "./Feature";
+import "./FeatureDeck.css"
+
+class FeatureDeck extends Component {
+    static defaultProps = {
+    features: [
+        { name: "Feature1", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature2", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature3", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature4", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature5", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature6", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature7", des: "Lorem ipsum dolor sit aret" },
+        { name: "Feature8", des: "Lorem ipsum dolor sit aret" }
+    ]
+};
+    render() {
+        return (
+            <div className="FeatureDeck">
+                <h1>Features</h1>
+                <div className="FeatureDeck-cards">
+                    {this.props.features.map((f) => (
+                        <Feature name={f.name} des={f.des}/>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FeatureDeck;

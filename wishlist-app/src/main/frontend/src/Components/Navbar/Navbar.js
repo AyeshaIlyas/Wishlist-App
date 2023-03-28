e39e96d8dc5ead46e860c3d6f196fa8e692034a4
Ayesha Ilyas <annie16950@gmail.com>
2023-03-28 15:31:24 -0400

    Delete Logout component and change navbar to functional component

--- wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
+++ wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
@@ -3,9 +3,15 @@ import { NavLink, Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "./logo.png";
 import AuthContext from "../Contexts/AuthContext";
+import Cookies from "js-cookie";
 
 export default function Navbar() {
     const authState = useContext(AuthContext);
+    
+    const logout = () => {
+        Cookies.remove("auth-session");
+        authState.setIsLoggedIn(false);
+    }
 
     return authState.isLoggedIn ? 
         (
@@ -16,7 +22,7 @@ export default function Navbar() {
                     <NavLink key="wishlists" to="/wishlists">Wishlists</NavLink>
                     <NavLink key="shared" to="/shared">Shared</NavLink>
                 </div>
-                <button className="Navbar-logout" onClick={this.props.logout}>Logout</button>
+                <button className="Navbar-logout" onClick={logout}>Logout</button>
             </nav>
         ) : (
             <nav className="Navbar">

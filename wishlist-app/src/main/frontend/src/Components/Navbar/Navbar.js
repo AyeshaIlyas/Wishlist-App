cdf2ec42179d0d9bee7e0e4b208e93a0d381f44b
Ayesha Ilyas <annie16950@gmail.com>
2023-03-28 15:08:45 -0400

    Merge frontend changes

--- wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
+++ wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
@@ -1,40 +1,30 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "./logo.png";
+import AuthContext from "../Contexts/AuthContext";
 
-class Navbar extends Component {
+export default function Navbar() {
+    const authState = useContext(AuthContext);
 
-    createNav() {
-        if (this.props.isLoggedIn) {
-            return (
-                <nav className="Navbar">
-                    <Link to="/"><img id="Navbar-logo" src={logo} alt="genie-logo"/></Link>
-                    <div className="Navbar-links">
-                        <NavLink key="profile" to="/profile">Profile</NavLink>
-                        <NavLink key="wishlists" to="/wishlists">Wishlists</NavLink>
-                        <NavLink key="shared" to="/shared">Shared</NavLink>
-                    </div>
-                    <button className="Navbar-logout" onClick={this.props.logout}>Logout</button>
-                </nav>
-            );
-        } 
-        return (
+    return authState.isLoggedIn ? 
+        (
+            <nav className="Navbar">
+                <Link to="/"><img id="Navbar-logo" src={logo} alt="genie-logo"/></Link>
+                <div className="Navbar-links">
+                    <NavLink key="profile" to="/profile">Profile</NavLink>
+                    <NavLink key="wishlists" to="/wishlists">Wishlists</NavLink>
+                    <NavLink key="shared" to="/shared">Shared</NavLink>
+                </div>
+                <button className="Navbar-logout" onClick={this.props.logout}>Logout</button>
+            </nav>
+        ) : (
             <nav className="Navbar">
                 <Link to="/"><img id="Navbar-logo" src={logo} alt="genie-logo"/></Link>
                 <h1 className="Navbar-title">Fourth Wish</h1>
             </nav>
         );
-            
-    }
-
-    render() {
-        return (
-            <>
-             {this.createNav()}
-            </>
-        );
-    }
+           
 }
 
-export default Navbar;
+

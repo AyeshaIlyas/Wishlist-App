6f0126614f157f708a4c6a8167863797c5dbd36f
Ayesha Ilyas <annie16950@gmail.com>
2023-04-03 21:33:18 -0400

    Update Profile.js

--- wishlist-app/src/main/frontend/src/Components/Profile/Profile.js
+++ wishlist-app/src/main/frontend/src/Components/Profile/Profile.js
@@ -1,35 +1,40 @@
 import { useContext, useEffect, useState } from 'react';
 import './Profile.css';
-import {getToken} from "./../../services/utils";
+import {authWrapper} from "./../../services/utils";
 import { getProfile } from '../../services/profileService';
 import AuthContext from '../Contexts/AuthContext';
 
 export default function Profile() {
-    const [profileInfo, setProfileInfo] = useState();
-    const authState = useContext(AuthContext);
+    const [profileInfo, setProfileInfo] = useState(null);
+    const {setIsLoggedIn} = useContext(AuthContext);
     
     useEffect(
         () => {
             const makeRequest = async () => {
-                const token = await getToken(authState.setIsLoggedIn);
-                console.log(token);
-                const profile = await getProfile(token);
-                setProfileInfo(profile)
+                const safeGetProfile = authWrapper(setIsLoggedIn, getProfile);
+                const profile = await safeGetProfile();
+                if (profile) {
+                    setProfileInfo(profile);
+                }
             }
             makeRequest();
-        }, [authState, setProfileInfo]
+        }, [setIsLoggedIn]
     );
 
 
     return (
         <section>
             <h1>PROFILE</h1>
-            { profileInfo && 
-            <>
-                <p>{profileInfo.firstName} {profileInfo.lastName}</p>
-                <p>Email: {profileInfo.email}</p>
-            </>
+            { profileInfo ? 
+                (
+                    <>
+                        <p>{profileInfo.firstName} {profileInfo.lastName}</p>
+                        <p>Email: {profileInfo.email}</p>
+                    </>
+                )
+                : <p>Loading profile info...</p>
             }
+
         </section>
     );
 }

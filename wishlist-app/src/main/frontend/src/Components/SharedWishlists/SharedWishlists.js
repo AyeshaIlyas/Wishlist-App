2c666662c8428233c5b36743875ff8b867bf298a
Ayesha Ilyas <annie16950@gmail.com>
2023-04-03 21:30:36 -0400

    Move SharedWishlists.js

---
+++ wishlist-app/src/main/frontend/src/Components/SharedWishlists/SharedWishlists.js
@@ -0,0 +1,13 @@
+import "./SharedWishlists.css";
+
+export default function SharedWishlists() {
+    return (
+        <div className="wishlistContainer">
+            <h1>Shared Wishlists</h1>
+            <ul>
+                <li>Jane Doe - My Birthday</li>
+                <li>Brenda Johnson - Baby Shower</li>
+            </ul>
+        </div>
+    );
+}

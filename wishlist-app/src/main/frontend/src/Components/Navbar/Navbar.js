044c2e42a57631310d70cf9ed36413713467b322
013MSI <69552976+013MSI@users.noreply.github.com>
2023-04-25 17:10:33 -0400

    User Info in Navbar

--- wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
+++ wishlist-app/src/main/frontend/src/Components/Navbar/Navbar.js
@@ -1,20 +1,46 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 import logo from "./logo.png";
 import AuthContext from "../Contexts/AuthContext";
 import Cookies from "js-cookie";
+import {authWrapper} from "./../../services/utils";
+import { getProfile } from '../../services/profileService';
 
 export default function Navbar() {
     const authState = useContext(AuthContext);
     const navigate = useNavigate();
-    
+    const [profileInfo, setProfileInfo] = useState({});
+
+    useEffect(
+        () => {
+            const makeRequest = async () => {
+                const safeGetProfile = authWrapper(authState.setIsLoggedIn, getProfile);
+                const profile = await safeGetProfile();
+                if (profile) {
+                    setProfileInfo(profile);
+                }
+            }
+            makeRequest();
+        }, [authState]
+    );
+
+    const displayProfile = () => {
+        return(
+            <div className="Navbar-profile-info">
+                <p>{profileInfo.firstName} {profileInfo.lastName}</p>
+                <p> {profileInfo.email}</p>               
+            </div> 
+        );      
+    }
+
     const logout = () => {
         Cookies.remove("auth-session");
         sessionStorage.removeItem("token");
         authState.setIsLoggedIn(false);
         navigate("/login");
     }
+
     return authState.isLoggedIn ? 
         (
             <nav className="Navbar">
@@ -27,7 +53,11 @@ export default function Navbar() {
                     <NavLink key="wishlists" to="/wishlists">Wishlists</NavLink>
                     <NavLink key="shared" to="/shared">Shared</NavLink>
                 </div>
-                <button className="Navbar-logout" onClick={logout}>Logout</button>
+                <div className="Navbar-userInfo">
+                    {displayProfile()}
+                    <button className="Navbar-logout" onClick={logout}>Logout</button>
+                </div>
+
             </nav>
         ) : (
             <nav className="Navbar"> 

3321424f4891c25e7583b69d9ba108f5702dbfd3
tyler nguyen <nguyent40@my.sunyulster.edu>
2023-04-27 00:08:08 -0400

    Revert "SharedWislistCard.js"
    This reverts commit 9e84b33d6ff05ab1ca8a16d2481aa9c6c9469c3a.

--- wishlist-app/src/main/frontend/src/Components/SharedWishlistCard/SharedWishlistCard.js
+++ wishlist-app/src/main/frontend/src/Components/SharedWishlistCard/SharedWishlistCard.js
@@ -1,15 +1,7 @@
 import { Link } from "react-router-dom";
 import "./SharedWishlistCard.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import ConfirmationDialog from "../Utils/ConfirmationDialog/ConfirmationDialog";
 
-
-export default function SharedWishlistCard({id, name, owner, itemCount, removeSelf}) {
-
-        const handleRemove = () => {
-           
-        }
-
+export default function SharedWishlistCard({id, name, owner, itemCount}) {
    return(
         <div className="SharedWishlistCard">
             <Link className="SharedWishlistCard-link" to={`/shared/${id}`}>
@@ -18,11 +10,7 @@ export default function SharedWishlistCard({id, name, owner, itemCount, removeSe
                         <h3 className="SharedWishlistCard-info SharedWishlist-owner">{owner}</h3>
                         <h3 className="SharedWishlistCard-info SharedWishlist-title">{name}</h3>
                         <h4 className="SharedWishlistCard-info SharedWishlist-itemCount">Items: {itemCount}</h4>
-                        <h5 className="SharedWishlistCard-info SharedWishlist-removeSelf"></h5>
-                            <button id="SharedWishlist-removeSelf" onClick="handleRemove"> <FontAwesomeIcon className="removeSelf" icon="fa-solid fa-x" /></button>
-                            
                     </div>
-                    {showDialog && <ConfirmationDialog title="Remove yourself from Shared Wishlist" details={`Are you sure you want remove yourself from this shared wishlist? ${deleteItem.name}?`} actionLabel="Remove" action={remove} cancel={cancelRemove}/>}
                 </header>
             </Link>
         </div>
